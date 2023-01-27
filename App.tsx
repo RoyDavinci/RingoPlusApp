46f9815dd83aa31f6d9e9f2b75ede769c6292cb3
DavinciCode <emsthias33@gmail.com>
2023-01-27 19:12:17 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View
+      style={{
+        backgroundColor: '#fff',
+      }}>
+      <Text>Welcome To A New App</Text>
+    </View>
+  );
+}
+
+export default App;
